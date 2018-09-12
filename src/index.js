cbfb2d3c254834b1f0ce42dfc65a0f63b1a9b29f
Austin Barnes <austin@austinbarnes.net>
2018-09-11 22:45:33 -0400

    updates to websocket

--- src/index.js
+++ src/index.js
@@ -5,9 +5,6 @@ import BluetoothConnector from './bluetooth/BluetoothConnector'
 import RealDataConnector from './connectors/powerfc/SerialConnector'
 import MockDataConnector from './mock/MockDataConnector'
 import { startServer } from './websocket/websocketController'
-import SerialPort from 'serialport'
-
-import os from "os"
 
 const shouldUseMockData = process.argv.includes("-mock") || process.argv.includes("mock");
 
--- src/utils/WebsocketUtil.js
+++ src/utils/WebsocketUtil.js
@@ -1,10 +1,16 @@
 export const wsData = (command, payload) => {
-    return {
+    return JSON.stringify({
         command: command,
         payload: payload
-    }
+    })
 }
 
 export const validateWsMessage = (data) => {
+    try {
+        data = JSON.parse(data);
+    } catch (e) {
+        return false
+    }
+    
     return (typeof data.command !== "undefined" && typeof data.payload !== "undefined");
 }
