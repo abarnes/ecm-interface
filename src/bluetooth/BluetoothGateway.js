6623759aeb3276153674f80d55eaaa30c3f60011
Austin Barnes <austin@austinbarnes.net>
2019-01-19 16:33:55 -0500

    reverted bleno-mac changes

--- src/bluetooth/BluetoothGateway.js
+++ src/bluetooth/BluetoothGateway.js
@@ -1,3 +1,4 @@
-const bleno = process.platform === "darwin" ? require('bleno-mac') : require('bleno');
+// const bleno = process.platform === "darwin" ? require('bleno-mac') : require('bleno');
+const bleno = require('bleno');
 
 module.exports = bleno;
