7a3fb36ae1ad8bcf77010bc31a39f1a94fa83c62
Austin Barnes <austin@austinbarnes.net>
2019-02-13 07:54:46 -0500

    testing bluetooth callback updates

--- src/bluetooth/characteristics/GaugeLayoutConfig.js
+++ src/bluetooth/characteristics/GaugeLayoutConfig.js
@@ -11,7 +11,7 @@ let bluetoothCallback = null;
 let GaugeLayoutConfigCharacteristic = function() {
     GaugeLayoutConfigCharacteristic.super_.call(this, {
         uuid: '94667c9c-6888-41a6-9401-3655ebbfaf63',
-        properties: ['write', 'read', 'notify']
+        properties: ['write', 'read']
     });
 };
 
@@ -24,16 +24,16 @@ GaugeLayoutConfigCharacteristic.prototype.onWriteRequest = function onWriteReque
         publishGaugeLayoutChange(newLayoutConfig);
     }
 
-    // Hack - use the notify callback here so that the value actually gets updated
     if (!withoutResponse && typeof bluetoothCallback === "function") {
         callback(BlenoCharacteristic.RESULT_SUCCESS);
-        bluetoothCallback(data);
+        // bluetoothCallback(data);
     }
 }
 
+/*
 GaugeLayoutConfigCharacteristic.prototype.onSubscribe = function(maxValueSize, updateValueCallback) {
     bluetoothCallback = updateValueCallback; 
-}
+}*/
 
 GaugeLayoutConfigCharacteristic.prototype.onReadRequest = function(offset, callback) {
     console.log("Bluetooth: GaugeLayoutConfigCharacteristic read");
