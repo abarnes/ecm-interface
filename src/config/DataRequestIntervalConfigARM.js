c6ee44fe589bfb9d9ddc07f84c59d2c00c2c5577
Austin Barnes <austin@austinbarnes.net>
2019-06-09 22:57:30 -0400

    updated ARM intervals

--- src/config/DataRequestIntervalConfigARM.js
+++ src/config/DataRequestIntervalConfigARM.js
@@ -3,8 +3,8 @@ import { PowerFCCommands } from '../connectors/powerfc/PowerFCCommands'
 const intervals = [
     {
         command: PowerFCCommands.basic,
-        requestInterval: 500,
-        stateUpdateSkips: 4,
+        requestInterval: 250,
+        stateUpdateSkips: 2,
         startOffset: 0
     }
 ];
--- src/config/DataRequestIntervalConfigX64.js
+++ src/config/DataRequestIntervalConfigX64.js
@@ -3,7 +3,7 @@ import { PowerFCCommands } from '../connectors/powerfc/PowerFCCommands'
 const intervals = [
     {
         command: PowerFCCommands.basic,
-        requestInterval: 250,
+        requestInterval: 10,
         stateUpdateSkips: 0,
         startOffset: 0
     },
