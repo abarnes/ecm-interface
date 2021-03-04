247bbb6cf04ef348271301ec8ace0905d955ba0c
Austin Barnes <austin@austinbarnes.net>
2021-03-03 20:24:22 -0500

    updates

--- src/config/DataRequestIntervalConfigX64.js
+++ src/config/DataRequestIntervalConfigX64.js
@@ -3,21 +3,21 @@ import { PowerFCCommands } from '../connectors/powerfc/PowerFCCommands'
 const intervals = [
     {
         command: PowerFCCommands.basic,
-        requestInterval: 1000,
+        requestInterval: 1000, // milliseconds
         stateUpdateSkips: 0,
         startOffset: 0
     },
     {
         command: PowerFCCommands.sensorData,
-        requestInterval: 2000,
+        requestInterval: 2000, // milliseconds
         stateUpdateSkips: 0,
-        startOffset: 250
+        startOffset: 250 // milliseconds, after app launch to start polling this piece
     },
     {
         command: PowerFCCommands.advancedData,
-        requestInterval: 1000,
+        requestInterval: 1000, // milliseconds
         stateUpdateSkips: 0,
-        startOffset: 500
+        startOffset: 500 // milliseconds, after app launch to start polling this piece
     }
 ];
 
